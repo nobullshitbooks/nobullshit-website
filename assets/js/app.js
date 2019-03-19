23d9c1d4d73ba0fefee5c235eb7ab5e8a3cd619b
Kevin <kevin56620@gmail.com>
2019-03-19 02:47:41 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+$('.js-toggle-menu').click(function() {
+  $('.l-header').toggleClass('is-collapse');
+});
+
+$('.js-smooth-scroll')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 80
+        }, 300);
+      }
+    }
+  });
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Jekyll
+
+Dev
+
+  'bundle exec jekyll serve --livereload'
+
+
+Build
+
+  'bundle exec jekyll build'
+
+  in /_site
